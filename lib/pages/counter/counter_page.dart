2d5de3305e1b0bbe38c28ab6f7f0f71c5cf29843
Reza Safavi <rezasfv36@gmail.com>
2025-02-20 12:58:46 +0100

    counter with AsyncNotifierProvider

--- lib/pages/counter/counter_page.dart
+++ lib/pages/counter/counter_page.dart
@@ -1,3 +1,4 @@
+import 'package:async1/pages/counter/counter_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,12 +7,56 @@ class CounterPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
+    print(counter);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter'),
-      ),
-      body: const Center(
-        child: Text('Counter'),
+      appBar: AppBar(title: const Text('Counter')),
+      body: Center(
+        child: counter.when(
+          skipLoadingOnRefresh: false,
+          data: (count) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('$count'),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () {
+                        ref.read(counterProvider.notifier).decrement();
+                      },
+                      child: Icon(Icons.remove),
+                    ),
+
+                    FloatingActionButton(
+                      onPressed: () {
+                        ref.read(counterProvider.notifier).increment();
+                      },
+                      child: Icon(Icons.add),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+          error:
+              (e, st) => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(e.toString()),
+                  SizedBox(height: 20),
+                  OutlinedButton(
+                    onPressed: () {
+                      ref.invalidate(counterProvider);
+                    },
+                    child: Text('Refresh'),
+                  ),
+                ],
+              ),
+
+          loading: () => CircularProgressIndicator(),
+        ),
       ),
     );
   }
