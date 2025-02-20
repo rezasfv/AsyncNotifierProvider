93d79251a51013ef811fef30ec2538765f4908bc
Reza Safavi <rezasfv36@gmail.com>
2025-02-20 15:06:55 +0100

    use code generation

--- lib/pages/counter/counter_provider.dart
+++ lib/pages/counter/counter_provider.dart
@@ -1,15 +1,71 @@
 import 'dart:async';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class Counter extends FamilyAsyncNotifier<int, int> {
+part 'counter_provider.g.dart';
+
+// class Counter extends FamilyAsyncNotifier<int, int> {
+//   @override
+//   FutureOr<int> build(int arg) async {
+//     ref.onDispose(() {
+//       print('[CounterProvider] disposed');
+//     });
+//     await waitSecond();
+//     return arg;
+//   }
+
+//   Future<void> waitSecond() => Future.delayed(const Duration(seconds: 1));
+
+//   Future<void> increment() async {
+//     state = const AsyncLoading();
+
+//     state = await AsyncValue.guard(() async {
+//       await waitSecond();
+//       if (state.value! == 2) {
+//         throw 'Fail to increment';
+//       }
+//       return state.value! + 1;
+//     });
+//     // try {
+//     //   await waitSecond();
+//     //   // throw 'Fail to increment';
+//     //   state = AsyncData(state.value! + 1);
+//     // } catch (error, stackTrace) {
+//     //   state = AsyncError(error, stackTrace);
+//     // }
+//   }
+
+//   Future<void> decrement() async {
+//     state = const AsyncLoading();
+
+//     state = await AsyncValue.guard(() async {
+//       await waitSecond();
+//       return state.value! - 1;
+//     });
+
+//     // try {
+//     //   await waitSecond();
+//     //   state = AsyncData(state.value! - 1);
+//     // } catch (error, stackTrace) {
+//     //   state = AsyncError(error, stackTrace);
+//     // }
+//   }
+// }
+
+// final counterProvider = AsyncNotifierProviderFamily<Counter, int, int>(
+//   Counter.new,
+// );
+
+@riverpod
+class Counter extends _$Counter {
   @override
-  FutureOr<int> build(int arg) async {
+  FutureOr<int> build(int initialValue) async {
     ref.onDispose(() {
       print('[CounterProvider] disposed');
     });
     await waitSecond();
-    return arg;
+    return initialValue;
   }
 
   Future<void> waitSecond() => Future.delayed(const Duration(seconds: 1));
@@ -24,13 +80,6 @@ class Counter extends FamilyAsyncNotifier<int, int> {
       }
       return state.value! + 1;
     });
-    // try {
-    //   await waitSecond();
-    //   // throw 'Fail to increment';
-    //   state = AsyncData(state.value! + 1);
-    // } catch (error, stackTrace) {
-    //   state = AsyncError(error, stackTrace);
-    // }
   }
 
   Future<void> decrement() async {
@@ -40,16 +89,5 @@ class Counter extends FamilyAsyncNotifier<int, int> {
       await waitSecond();
       return state.value! - 1;
     });
-
-    // try {
-    //   await waitSecond();
-    //   state = AsyncData(state.value! - 1);
-    // } catch (error, stackTrace) {
-    //   state = AsyncError(error, stackTrace);
-    // }
   }
 }
-
-final counterProvider = AsyncNotifierProviderFamily<Counter, int, int>(
-  Counter.new,
-);
